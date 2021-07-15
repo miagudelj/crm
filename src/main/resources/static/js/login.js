bcb9588813ab9a4e97304b9b09aa6e1288449813
Mia <mia.gudelj@gmx.ch>
2021-07-15 23:18:41 +0200

    deleted login function and changed url from delete

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -7,39 +7,13 @@
  */
 $(document).ready(
     function () {
-
-        $("#loginForm").submit(sendLogin);
         $("#logout").click(sendLogout);
-
     });
-
-function sendLogin(form) {
-    form.preventDefault();
-
-    $.ajax({
-        url: "./rest/v1/users/login",
-        type: "POST",
-        dataType: "text",
-        data: $("#loginForm").serialize()
-    })
-
-        .done(function (jsonData) {
-            window.location.href = "./index.html";
-        })
-
-        .fail(function (xhr, status, errorThrown) {
-            if (xhr.status == 404) {
-                $("#message").text("Benutzername/Passwort unbekannt");
-            } else {
-                $("#message").text("Es ist ein Fehler aufgetreten");
-            }
-        })
-}
 
 function sendLogout() {
 
     $.ajax({
-        url: "./rest/v1/user/logout",
+        url: "./rest/v1/user/",
         type: "DELETE",
         dataType: "text"
     })
